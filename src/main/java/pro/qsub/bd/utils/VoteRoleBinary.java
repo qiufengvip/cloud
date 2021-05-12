0b8a905b55fdc6f5e56ab2860253201bde6b3136
jzh233 <1147981135@qq.com>
2021-05-12 20:42:03 +0800

    策略接口—name二进制

---
+++ src/main/java/pro/qsub/bd/utils/VoteRoleBinary.java
@@ -0,0 +1,48 @@
+package pro.qsub.bd.utils;
+
+import pro.qsub.bd.entity.Server;
+import pro.qsub.bd.service.VoteRole;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author JZH
+ * @desc 选举规则-二进制算法
+ * @time 2021-05-12-20:11
+ */
+public class VoteRoleBinary implements VoteRole {
+
+
+    /**
+     * 二进制算法
+     * @param workingServes
+     * @return
+     */
+    @Override
+    public Server getMasterServer(List<Server> workingServes) {
+        //最终asc大小
+        int endName = 0;
+        Server server = null;
+        //遍历服务器集合
+        for (Server workingServe : workingServes) {
+            //获取服务器名称
+            String name = workingServe.getName();
+            //字符asc相加
+            int ascLen = 0;
+            for(int i = 0 ; i<name.length() ; i++){
+                char temp = name.charAt(i);
+                ascLen += Integer.valueOf(temp);
+            }
+            System.out.println("ascLen:"+ascLen);
+            System.out.println("endName:"+endName);
+            //判断后更改
+            if(ascLen > endName){
+                endName = ascLen;
+                server = workingServe;
+            }
+        }
+        return server;
+    }
+
+}
